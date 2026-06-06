15d67f96933b97fd4c1bacbd1ca8e72981a29042
Gnorgel <rijschroeder@gmail.com>
2026-06-07 00:35:56 +0200

    comments

--- src/clock/core.js
+++ src/clock/core.js
@@ -1,15 +1,16 @@
 export function getAktuelleZeitdaten() {
-    const jetzt = new Date();
-    const aktuellerMonat = jetzt.getMonth();
+    const jetzt = new Date(); //System uhr wird genutzt
+    const aktuellerMonat = jetzt.getMonth(); //fängt bei 0 an, deswegen unten + 1
     const aktuellesJahr = jetzt.getFullYear();
 
     //berechnung der tage im aktuellen Monat
-    const tageImMonat = new Date(aktuellesJahr, aktuellerMonat + 1, 0).getDate();
+    const tageImMonat = new Date(aktuellesJahr, aktuellerMonat + 1, 0).getDate(); 
+    //neues date objekt, tag wird 0, was als letzter tag des vorherigen monats zählt, dann wird aktueller tag geholt
 
     return {
         jahr: aktuellesJahr,
         monat: aktuellerMonat,
-        tag: jetzt.getDate() - 1, // -1 für 0-basierte Indexierung im Grid
+        tag: jetzt.getDate() - 1, // -1 für 0-basierte Indexierung im Grid, damit erstes element erster tag ist.
         stunde: jetzt.getHours(),
         minute: jetzt.getMinutes(),
         sekunde: jetzt.getSeconds(),
--- src/main.js
+++ src/main.js
@@ -1,15 +1,17 @@
-import { getAktuelleZeitdaten } from './clock/core.js';
+import { getAktuelleZeitdaten } from './clock/core.js'; //Importiert die variablen beider anderer js dateien
 import { renderZeitSpalte, renderSekundenKaskade } from './clock/renderer.js';
 
-document.addEventListener("DOMContentLoaded", () => {
-    // DOM-Elemente aus der index.html greifen
+//Verwaltungsklasse für unsere Logik Klassen
+
+document.addEventListener("DOMContentLoaded", () => { //Evenlistener
+    // DOM-Elemente aus der index.html greifen, mit ID
     const colMonate = document.getElementById("col-monate");
     const colTage = document.getElementById("col-tage");
     const colStunden = document.getElementById("col-stunden");
     const colMinuten = document.getElementById("col-minuten");
     const colSekunden = document.getElementById("col-sekunden");
 
-    // Die Herzschlag-Funktion der Uhr
+    // Die tick funktion von uhr 
     function tick() {
         // 1. Daten holen
         const zeitdaten = getAktuelleZeitdaten();
@@ -18,7 +20,7 @@ document.addEventListener("DOMContentLoaded", () => {
         renderSekundenKaskade(colSekunden, zeitdaten.sekunde);
         renderZeitSpalte(colMinuten, 60, zeitdaten.minute);
         renderZeitSpalte(colStunden, 24, zeitdaten.stunde);
-        renderZeitSpalte(colTage, zeitdaten.tageImMonat, zeitdaten.tag);
+        renderZeitSpalte(colTage, zeitdaten.tageImMonat, zeitdaten.tag); //Tage sind variabel
         renderZeitSpalte(colMonate, 12, zeitdaten.monat);
     }
 
