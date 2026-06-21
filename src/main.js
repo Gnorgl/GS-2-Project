7087f6acf5c6cbc900e81a00aeb86f0e2450cb8b
Gnorgel <rijschroeder@gmail.com>
2026-06-21 02:26:03 +0200

    new design

--- src/main.js
+++ src/main.js
@@ -1,30 +1,27 @@
-import { getAktuelleZeitdaten } from './clock/core.js'; //Importiert die variablen beider anderer js dateien
+import { getAktuelleZeitdaten } from './clock/core.js';
 import { renderZeitSpalte, renderSekundenKaskade } from './clock/renderer.js';
 
-//Verwaltungsklasse für Logik Klassen
-
-document.addEventListener("DOMContentLoaded", () => { //Eventlistener
-    // DOM-Elemente aus der index.html greifen, mit ID
+document.addEventListener("DOMContentLoaded", () => {
+    // DOM-Elemente greifen (Spindel-Referenz erfolgreich gelöscht!)
     const colMonate = document.getElementById("col-monate");
     const colTage = document.getElementById("col-tage");
     const colStunden = document.getElementById("col-stunden");
     const colMinuten = document.getElementById("col-minuten");
     const colSekunden = document.getElementById("col-sekunden");
 
-    // Die tick funktion von uhr 
+    // Die Herzschlag-Funktion der Uhr
     function tick() {
-        // 1. Daten holen
         const zeitdaten = getAktuelleZeitdaten();
 
-        // 2. Ansicht aktualisieren
+        // Ansicht aktualisieren
         renderSekundenKaskade(colSekunden, zeitdaten.sekunde);
         renderZeitSpalte(colMinuten, 60, zeitdaten.minute);
         renderZeitSpalte(colStunden, 24, zeitdaten.stunde);
-        renderZeitSpalte(colTage, zeitdaten.tageImMonat, zeitdaten.tag); //Tage sind variabel
+        renderZeitSpalte(colTage, zeitdaten.tageImMonat, zeitdaten.tag);
         renderZeitSpalte(colMonate, 12, zeitdaten.monat);
     }
 
-    // Sofort beim Laden einmal ausführen und dann jede Sekunde (1000ms) wiederholen
+    // Sofort starten und jede Sekunde wiederholen
     tick();
     setInterval(tick, 1000);
 });
